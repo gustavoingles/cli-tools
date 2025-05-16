a4fda981113eece269c68d7bda549c23bc1f377a
gustavoingles <gustavo_aboim_ingles@hotmail.com>
2025-05-16 12:26:42 -0300

    add sum operation

--- main.go
+++ main.go
@@ -1,30 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	calculator := calculator{}
 	fmt.Println("If you want to sum two numbers, type \"sum\", if you want to subtract two numbers, type \"subtract\", if you want to multiply two numbers, type \"multiply\", if you want to divide two numbers, type \"divide\"")
-	if os.Args[1] == "sum" {
-		for i, _ := range os.Args[2:] {
-			number := float64(i)
-			calculator.firstNumber = number
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	chooseOperation := scanner.Text()
+	if chooseOperation == "sum" {
+		scanner.Scan()
+		firstNumber := scanner.Text()
+		convFirstNumber, err := strconv.ParseFloat(firstNumber, 64)
+		if err != nil {
+			log.Printf("It's happened an error while parsing the chosen number, please verify it: %v", err)
 		}
+		calculator.firstNumber = convFirstNumber
+
+		scanner.Scan()
+		secondNumber := scanner.Text()
+		convSecondNumber, err := strconv.ParseFloat(secondNumber, 64)
+		if err != nil {
+			log.Printf("It's happened an error while parsing the chosen number, please verify it: %v", err)
+		}
+		calculator.secondNumber = convSecondNumber
+
+		totalSum := calculator.firstNumber + calculator.secondNumber
+
+		fmt.Printf("The sum between %f and %f is equals to %f\n", calculator.firstNumber, calculator.secondNumber, totalSum)
 	}
 }
 
 type calculator struct {
-	firstNumber float64
+	firstNumber  float64
 	secondNumber float64
 }
 
 type operations interface {
 	sum(x, y float64) float64
 	subtract(x, y float64) float64
-	multiply(x, y float64) float64 
+	multiply(x, y float64) float64
 	divide(x, y float64) float64
 }
 
