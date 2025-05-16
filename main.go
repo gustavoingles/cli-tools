f718b80dab13019fb6dd11cd08cde3ec66d8e918
gustavoingles <gustavo_aboim_ingles@hotmail.com>
2025-05-16 12:29:55 -0300

    add subtraction

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ func main() {
 	fmt.Println("If you want to sum two numbers, type \"sum\", if you want to subtract two numbers, type \"subtract\", if you want to multiply two numbers, type \"multiply\", if you want to divide two numbers, type \"divide\"")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	chooseOperation := scanner.Text()
-	if chooseOperation == "sum" {
+	chosenOperation := scanner.Text()
+	if chosenOperation == "sum" {
 		scanner.Scan()
 		firstNumber := scanner.Text()
 		convFirstNumber, err := strconv.ParseFloat(firstNumber, 64)
@@ -34,6 +34,26 @@ func main() {
 		totalSum := calculator.firstNumber + calculator.secondNumber
 
 		fmt.Printf("The sum between %f and %f is equals to %f\n", calculator.firstNumber, calculator.secondNumber, totalSum)
+	} else if chosenOperation == "subtract" {
+		scanner.Scan()
+		firstNumber := scanner.Text()
+		convFirstNumber, err := strconv.ParseFloat(firstNumber, 64)
+		if err != nil {
+			log.Printf("It's happened an error while parsing the chosen number, please verify it: %v", err)
+		}
+		calculator.firstNumber = convFirstNumber
+
+		scanner.Scan()
+		secondNumber := scanner.Text()
+		convSecondNumber, err := strconv.ParseFloat(secondNumber, 64)
+		if err != nil {
+			log.Printf("It's happened an error while parsing the chosen number, please verify it: %v", err)
+		}
+		calculator.secondNumber = convSecondNumber
+
+		totalSubtraction := calculator.firstNumber - calculator.secondNumber
+
+		fmt.Printf("The subtraction between %f and %f is equals to %f\n", calculator.firstNumber, calculator.secondNumber, totalSubtraction)
 	}
 }
 
